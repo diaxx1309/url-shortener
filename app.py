92755b3dd1a27a5736573a3689e154d87634bc6b
diya rai <diyarai869@gmail.com>
2026-05-23 23:55:06 +0530

    Sqlalchemy installed and introduced in flask

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
 import string,random
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL']='sqlite:///urls.db'
+db= SQLAlchemy(app)
 
 urls={}
 
